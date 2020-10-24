f79a0faccfbf653dc5023686c60f2ec063b60ec4
DamascenoRafael (via Travis CI) <damscn@gmail.com>
2020-10-24 23:56:06 +0000

    Deploy DamascenoRafael/pesc-collaboration-network to github.com/DamascenoRafael/pesc-collaboration-network.git:gh-pages

---
+++ precache-manifest.d3dae81f59266b2a2b0a17a525e20899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a25fa129e8c4821c5e18bd0453746c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdfed6c3fdd884fcf5ad",
+    "url": "/static/css/main.e77121a2.chunk.css"
+  },
+  {
+    "revision": "f50d8b9963951e1ed4c5",
+    "url": "/static/js/2.44958fe1.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.44958fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfed6c3fdd884fcf5ad",
+    "url": "/static/js/main.d7ec9d76.chunk.js"
+  },
+  {
+    "revision": "a1378c363741a146b198",
+    "url": "/static/js/runtime-main.742a90bb.js"
+  }
+]);
